63ff1e0d18277f84ff48f53c3322d14c65fd2f0d
pomeloshark <121037935+pomeloshark@users.noreply.github.com>
2023-01-07 15:51:19 -0400

    Init commit

---
+++ lib/jekyll/ipa.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "ipa/version"
+
+module Jekyll
+  module Ipa
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/jekyll/ipa/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Jekyll
+  module Ipa
+    VERSION = "0.1.0"
+  end
+end
